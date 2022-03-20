d86e7129b8f3157eca6a212915c3f5e71cfe5899
kosmos <3apa3a.iickov@gmail.com>
2022-03-20 22:04:01 +0300

    feat: add dark mode, add functions calculated

--- src/components/CCalculator.vue
+++ src/components/CCalculator.vue
@@ -4,34 +4,36 @@
       <CSwitch />
     </div>
     <div class="calculator__display">
-      <div class="calculator__display-history">{{ history }}</div>
-      <div class="calculator__display-sum">{{ sum }}</div>
+      <div v-if="selectedOperation" class="calculator__display-history">
+        {{ prevNum }} {{ selectedOperation }} {{ currentNum }}
+      </div>
+      <div class="calculator__display-sum">{{ currentNum }}</div>
     </div>
     <div class="calculator__buttons">
-      <CButton value="C" type="medium"></CButton>
-      <CButton type="medium"></CButton>
-      <CButton value="%" type="medium"></CButton>
-      <CButton value="÷" type="high"></CButton>
+      <CButton value="C" type="medium" @click-button="pressed('c')"></CButton>
+      <CButton type="medium" @click-button="negateValue"></CButton>
+      <CButton value="%" type="medium" @click-button="pressed('%')"></CButton>
+      <CButton value="÷" type="high" @click-button="pressed('/')"></CButton>
 
-      <CButton value="7" type="low"></CButton>
-      <CButton value="8" type="low"></CButton>
-      <CButton value="9" type="low"></CButton>
-      <CButton value="×" type="high"></CButton>
+      <CButton value="7" type="low" @click-button="pressed('7')"></CButton>
+      <CButton value="8" type="low" @click-button="pressed('8')"></CButton>
+      <CButton value="9" type="low" @click-button="pressed('9')"></CButton>
+      <CButton value="×" type="high" @click-button="pressed('*')"></CButton>
 
-      <CButton value="4" type="low"></CButton>
-      <CButton value="5" type="low"></CButton>
-      <CButton value="6" type="low"></CButton>
-      <CButton value="–" type="high"></CButton>
+      <CButton value="4" type="low" @click-button="pressed('4')"></CButton>
+      <CButton value="5" type="low" @click-button="pressed('5')"></CButton>
+      <CButton value="6" type="low" @click-button="pressed('6')"></CButton>
+      <CButton value="–" type="high" @click-button="pressed('-')"></CButton>
 
-      <CButton value="1" type="low"></CButton>
-      <CButton value="2" type="low"></CButton>
-      <CButton value="3" type="low"></CButton>
-      <CButton value="+" type="high"></CButton>
+      <CButton value="1" type="low" @click-button="pressed('1')"></CButton>
+      <CButton value="2" type="low" @click-button="pressed('2')"></CButton>
+      <CButton value="3" type="low" @click-button="pressed('3')"></CButton>
+      <CButton value="+" type="high" @click-button="pressed('+')"></CButton>
 
-      <CButton value="." type="low"></CButton>
-      <CButton value="0" type="low"></CButton>
-      <CButton type="low"></CButton>
-      <CButton value="=" type="high"></CButton>
+      <CButton value="." type="low" @click-button="pressed('.')"></CButton>
+      <CButton value="0" type="low" @click-button="pressed('0')"></CButton>
+      <CButton type="low" @click-button="deleteValue"></CButton>
+      <CButton value="=" type="high" @click-button="pressed('=')"></CButton>
     </div>
   </div>
 </template>
@@ -39,19 +41,114 @@
 <script>
 import CButton from "@/components/CButton.vue";
 import CSwitch from "@/components/CSwitch.vue";
-import { ref } from "vue";
+import { onMounted, ref, defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: "CCalculator",
   components: { CSwitch, CButton },
   setup() {
-    const sum = ref("");
-    const history = ref("");
+    const operations = ["+", "-", "*", "/", "%", "()"];
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+    const currentNum = ref("");
+    const prevNum = ref("");
+    const selectedOperation = ref("");
+    const total = ref("");
+    const pressed = (value) => {
+      if (value === "=" || value === "Enter") {
+        historyData.push({
+          operations: `${prevNum.value} ${selectedOperation.value} ${currentNum.value}`,
+          sum: eval(`${prevNum.value}
+          ${selectedOperation.value}
+          ${currentNum.value}`),
+        });
+        calculate();
+      } else if (value === "%") percentage();
+      else if (value === "c") clear();
+      else if (operations.includes(value)) applyOperation(value);
+      else if (numbers.includes(value)) appendNumber(value);
+    };
+    const appendNumber = (value) => {
+      currentNum.value = currentNum.value + value;
+    };
+    const applyOperation = (value) => {
+      calculate();
+      prevNum.value = currentNum.value;
+      currentNum.value = "";
+      selectedOperation.value = value;
+    };
+    const calculate = () => {
+      if (selectedOperation.value === "*") multiply();
+      if (selectedOperation.value === "()") multiply();
+      else if (selectedOperation.value === "/") divide();
+      else if (selectedOperation.value === "%") percentage();
+      else if (selectedOperation.value === "-") subtract();
+      else if (selectedOperation.value === "+") sum();
+      prevNum.value = "";
+      selectedOperation.value = "";
+    };
+    const multiply = () => {
+      currentNum.value = prevNum.value * currentNum.value;
+      total.value = currentNum.value;
+    };
+    const divide = () => {
+      currentNum.value = prevNum.value / currentNum.value;
+      total.value = currentNum.value;
+    };
+    const percentage = () => {
+      currentNum.value = currentNum.value / 100;
+      total.value = currentNum.value;
+    };
+    const subtract = () => {
+      currentNum.value = prevNum.value - currentNum.value;
+      total.value = currentNum.value;
+    };
+    const sum = () => {
+      currentNum.value = +prevNum.value + +currentNum.value;
+      total.value = currentNum.value;
+    };
+    const clear = () => {
+      currentNum.value = "";
+      prevNum.value = "";
+      selectedOperation.value = "";
+      currentNum.value = "";
+    };
+    const deleteValue = () => {
+      if (currentNum.value.length !== 0 && currentNum.value !== "0") {
+        currentNum.value = currentNum.value.slice(0, -1);
+      }
+    };
+    const negateValue = () => {
+      if (currentNum.value === "0") {
+        return (currentNum.value = "-0");
+      }
+      if (currentNum.value === "-") {
+        return (currentNum.value = "0");
+      }
+      if (currentNum.value === "0.") {
+        return (currentNum.value = `-${currentNum.value}`);
+      }
+      if (currentNum.value === "-0.") {
+        return (currentNum.value = "0.");
+      }
+      currentNum.value = `${currentNum.value * -1}`;
+    };
+    let historyData = [];
+
+    const handleKeydown = (e) => {
+      pressed(e.key);
+    };
+    onMounted(() => window.addEventListener("keydown", handleKeydown));
 
     return {
-      sum,
-      history,
+      currentNum,
+      pressed,
+      selectedOperation,
+      prevNum,
+      negateValue,
+      deleteValue,
+      total,
+      historyData,
     };
   },
-};
+});
 </script>
--- src/components/CSwitch.vue
+++ src/components/CSwitch.vue
@@ -1,9 +1,59 @@
-<template><div></div></template>
+<template>
+  <label class="calculator-switch">
+    <span class="calculator-switch__titles">
+      <span class="calculator-switch__title">Dark</span>
+      <span class="calculator-switch__title">Light</span>
+    </span>
+    <input
+      type="checkbox"
+      class="calculator-switch__input"
+      v-model="themeMode"
+    />
+    <span class="calculator-switch__toggle"></span>
+  </label>
+</template>
 
-<script>
+<script lang="ts">
+import { computed, onMounted, ref } from "vue";
+
+enum ThemModeValue {
+  Dark = "Dark",
+  Light = "Light",
+}
+
 export default {
   name: "CSwitch",
+  setup() {
+    const themeMode = ref<boolean>(false);
+
+    // eslint-disable-next-line vue/return-in-computed-property
+    const changeTheme = computed(() => {
+      const html = document.getElementsByTagName("html")[0];
+
+      if (themeMode.value) {
+        html.classList.add("light-mode");
+        window.localStorage.setItem("theme-mode", "Light");
+      } else {
+        html.classList.remove("light-mode");
+        window.localStorage.setItem("theme-mode", ThemModeValue.Dark);
+      }
+    });
+
+    onMounted(() => {
+      const selectedMode = window.localStorage.getItem("theme-mode");
+      if (selectedMode === ThemModeValue.Light) {
+        themeMode.value = true;
+      } else if (selectedMode === ThemModeValue.Dark) {
+        themeMode.value = false;
+      } else {
+        window.localStorage.setItem("theme-mode", ThemModeValue.Dark);
+      }
+    });
+
+    return {
+      themeMode,
+      changeTheme,
+    };
+  },
 };
 </script>
-
-<style scoped></style>
